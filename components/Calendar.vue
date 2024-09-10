79a69716c51f68744b544e45ffda34f0af860441
NextFire <git@yuru.moe>
2024-09-10 23:19:26 +0200

    calendar: frlocale

--- components/Calendar.vue
+++ components/Calendar.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { CalendarOptions } from "@fullcalendar/core/index.js";
+import frLocale from "@fullcalendar/core/locales/fr";
 import iCalendarPlugin from "@fullcalendar/icalendar";
 import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
@@ -13,6 +14,7 @@ const options: CalendarOptions = {
   height: "calc(100vh - 175px)",
   nowIndicator: true,
   scrollTime: "21:00:00",
+  locale: frLocale,
   ...props,
 };
 </script>
