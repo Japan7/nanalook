1980622e1652ce9cb14dac2b9ffe6f1aa7576cf5
NextFire <git@yuru.moe>
2024-12-03 11:50:29 +0100

    ci: check job

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
-  typescript: {
-    typeCheck: true,
-  },
   runtimeConfig: {
     nanapiUrl: "",
     nanapiClientUsername: "",
